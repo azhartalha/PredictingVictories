7499b62c7ea43c2dc53fbb0b676925396448d40f
Azhar Talha <syedazhartalha@gmail.com>
2022-03-16 23:04:32 -0400

    Refactored and added new features

--- model_training.py
+++ model_training.py
@@ -17,23 +17,30 @@ with open('y.pkl', 'rb') as f:
 x = tf.keras.preprocessing.sequence.pad_sequences(x, padding="post", value=99.0, dtype='float32')
 y = np.array(y, dtype='float32')
 
-features = 8 # Health points of 8 units
-time_steps = 119 # Max time steps
+features = 8 * 5 # Health points of 8 units
+time_steps = 521 # Max time steps
 
 model = Sequential()
 model.add(tf.keras.layers.Masking(mask_value=99.0, input_shape=(time_steps, features)))
-model.add(LSTM(32, return_sequences=False))
+model.add(LSTM(64, return_sequences=False))
 model.add(Dense(1, activation='sigmoid'))
 
 model.compile(loss='binary_crossentropy',
               optimizer='adam', metrics=['accuracy'])
 
-history = model.fit(x, y, validation_split=0.2, epochs=50, batch_size=32)
+history = model.fit(x, y, validation_split=0.2, epochs=20, batch_size=128)
 
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
 plt.xlabel("Epochs")
 plt.ylabel("Accuracy")
 plt.legend(["training_acc", "validation_acc"])
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+plt.legend(["training_loss", "validation_loss"])
 plt.show()
 
